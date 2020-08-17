d5560c597879c07b8c89a064a35fa9dbf81c2cb0
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-17 15:43:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,10 +6,19 @@ def loop_message_five_times(message)
     n+=1
   end
 end
+
 def loop_message_n_times(message, number)
   counter = 0
   while counter < number do
     puts message
     counter += 1
+  end
+end
+
+def output_array(array)
+  count = 0
+    while count < array.length do
+      puts array[count]
+      count += 1
   end
 end
